7dd9d1a75e28a5190d8a2959b71635e7a817e4ff
andras <andra.stancicu@teamnet.ro>
2015-03-16 12:04:34 +0200

    remove WAPIExtended

--- wfmc-commons/src/main/java/org/wfmc/service/WfmcService.java
+++ wfmc-commons/src/main/java/org/wfmc/service/WfmcService.java
@@ -1,9 +1,11 @@
 package org.wfmc.service;
 
+import org.wfmc.wapi2.WAPI2;
+
 /**
  * Created by Lucian.Dragomir on 2/10/2015.
  */
-public interface WfmcService extends WAPIExtended, Service, XpdlInterface {
+public interface WfmcService extends Service, XpdlInterface, WAPI2 {
 
     public String getServiceInstance();
 }
