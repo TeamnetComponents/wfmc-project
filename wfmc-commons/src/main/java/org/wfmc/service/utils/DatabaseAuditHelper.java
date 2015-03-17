f8541cafb627310ec6cb0050fa01662805c421b6
Ioan.Ivan <Ioan.Ivan@teamnet.ro>
2015-03-17 15:01:18 +0200

    WFMCELO-61 cod JDBC pentru insert - WMS_AUDIT_BASE

--- wfmc-commons/src/main/java/org/wfmc/service/utils/DatabaseAuditHelper.java
+++ wfmc-commons/src/main/java/org/wfmc/service/utils/DatabaseAuditHelper.java
@@ -1,8 +1,11 @@
 package org.wfmc.service.utils;
 
-import javax.activation.DataSource;
-
 import org.wfmc.audit.WMACreateProcessInstanceData;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 /**
  * Created by andras on 3/17/2015.
@@ -10,6 +13,39 @@ import org.wfmc.audit.WMACreateProcessInstanceData;
 public class DatabaseAuditHelper {
 
     public void insertCreateProcessInstanceAudit(DataSource dataSource, WMACreateProcessInstanceData wmaCreateProcessInstanceData){
-        //todo Mihai
+        PreparedStatement preparedStatement = null;
+        try {
+            Connection connection = dataSource.getConnection();
+
+
+            preparedStatement = connection.prepareStatement("INSERT INTO WMAAuditEntry  (ID,processDefinitionId, " +
+                    " activityDefinitionId,  initialProcessInstanceId,  currentProcessInstanceId,  activityInstanceId,  \n" +
+                    "                          workItemId,  processState,  eventCode,  domainId,  nodeId,  userId, roleId,  time,  informationId)  VALUES\n" +
+                    "  (WMAAuditEntry_Sequence.nextval,?,?," +
+                    "?,?,?,?,?,?,?,?,?,?,SYSDATE,?)");
+            preparedStatement.setString(1,wmaCreateProcessInstanceData.getProcessDefinitionId());
+            preparedStatement.setString(2,wmaCreateProcessInstanceData.getActivityDefinitionId());
+            preparedStatement.setString(3,wmaCreateProcessInstanceData.getInitialProcessInstanceId());
+            preparedStatement.setString(4,wmaCreateProcessInstanceData.getCurrentProcessInstanceId());
+            preparedStatement.setString(5,wmaCreateProcessInstanceData.getActivityInstanceId());
+            preparedStatement.setString(6,wmaCreateProcessInstanceData.getWorkItemId());
+            preparedStatement.setString(7,wmaCreateProcessInstanceData.getProcessState());
+            preparedStatement.setInt(8,wmaCreateProcessInstanceData.getEventCode().value());
+            preparedStatement.setString(9,wmaCreateProcessInstanceData.getDomainId());
+            preparedStatement.setString(10,wmaCreateProcessInstanceData.getNodeId());
+            preparedStatement.setString(11,wmaCreateProcessInstanceData.getUserId());
+            preparedStatement.setString(12,wmaCreateProcessInstanceData.getRoleId());
+            preparedStatement.setString(13,wmaCreateProcessInstanceData.getInformationId());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            if(preparedStatement!=null)
+                try {
+                    preparedStatement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+        }
     }
 }
--- wfmc-commons/src/test/java/org/wfmc/service/WfmcServiceFactoryTest.java
+++ wfmc-commons/src/test/java/org/wfmc/service/WfmcServiceFactoryTest.java
@@ -1,13 +1,17 @@
 package org.wfmc.service;
 
+import org.fest.assertions.Assertions;
+import org.junit.Test;
+import org.wfmc.audit.WMACreateProcessInstanceData;
+import org.wfmc.audit.WMAEventCode;
+import org.wfmc.service.utils.DatabaseAuditHelper;
+
+import javax.sql.DataSource;
 import java.io.IOException;
 import java.sql.SQLException;
-import java.util.*;
-
-import org.fest.assertions.Assertions;
-
-import org.junit.Test;
-
+import java.util.Enumeration;
+import java.util.Properties;
+import java.util.ResourceBundle;
 
 
 /**
@@ -46,6 +50,44 @@ public class WfmcServiceFactoryTest {
         WfmcServiceFactory wfmcServiceFactory = new WfmcServiceFactory(convertResourceBundleToProperties(configBundle));
         WfmcServiceAuditImpl wfmcService = (WfmcServiceAuditImpl)wfmcServiceFactory.getInstance();
         Assertions.assertThat(wfmcService.getDataSource().getConnection()).isNotNull();
+    }
+
+    @Test
+    public void testInsertCreateProcessInstanceAudit()
+            throws ClassNotFoundException, IOException, InstantiationException, IllegalAccessException, SQLException {
+
+
+        DatabaseAuditHelper databaseAuditHelper = new DatabaseAuditHelper();
+
+
+
+        WMACreateProcessInstanceData w = new WMACreateProcessInstanceData();
+        w.setProcessDefinitionId("procDefId4");
+        w.setInitialProcessInstanceId("iniprocId");
+        w.setActivityDefinitionId("actDefId");
+        w.setCurrentProcessInstanceId("procInsId");
+        w.setActivityInstanceId("actInsId");
+        w.setWorkItemId("wiId");
+        w.setProcessState(null);
+        w.setEventCode(WMAEventCode.WAITING_ON_EVENT);
+        w.setNodeId("nodeId");
+        w.setUserId("userId");
+        w.setRoleId("roleId");
+        w.setDomainId("domid");
+
+        w.setTimestamp(null);
+        w.setInformationId("infId");
+
+
+        //apel insert
+
+        ResourceBundle configBundle = ResourceBundle.getBundle("wapi-elo-renns");
+        WfmcServiceFactory wfmcServiceFactory = new WfmcServiceFactory(convertResourceBundleToProperties(configBundle));
+        WfmcServiceAuditImpl wfmcService = (WfmcServiceAuditImpl)wfmcServiceFactory.getInstance();
+        DataSource dS = wfmcService.getDataSource();
+        Assertions.assertThat(wfmcService.getDataSource().getConnection()).isNotNull();
+        databaseAuditHelper.insertCreateProcessInstanceAudit(dS,w);
+
     }
 
     static Properties convertResourceBundleToProperties(ResourceBundle resource) {
