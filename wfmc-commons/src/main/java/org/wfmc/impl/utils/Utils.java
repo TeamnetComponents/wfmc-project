3c68a3620f8dfdb930548287f8a5ebe0f20b72c6
andras <andra.stancicu@teamnet.ro>
2015-03-17 16:55:47 +0200

    fix issues from sonar - coretare nume constante (cu capslock si _ )

--- wfmc-commons/src/main/java/org/wfmc/impl/utils/Utils.java
+++ wfmc-commons/src/main/java/org/wfmc/impl/utils/Utils.java
@@ -9,18 +9,18 @@ import java.util.Map;
  * Created by Lucian.Dragomir on 3/4/2015.
  */
 public class Utils {
-    private static final String itemSeparator = ";";
-    private static final String valueSeparator = "=";
-    private static final String itemSeparatorQuoted = java.util.regex.Matcher.quoteReplacement(itemSeparator);
-    private static final String valueSeparatorQuoted = java.util.regex.Matcher.quoteReplacement(valueSeparator);
+    private static final String ITEM_SEPARATOR = ";";
+    private static final String VALUE_SEPARATOR = "=";
+    private static final String ITEM_SEPARATOR_QUOTED = java.util.regex.Matcher.quoteReplacement(ITEM_SEPARATOR);
+    private static final String VALUE_SEPARATOR_QUOTED = java.util.regex.Matcher.quoteReplacement(VALUE_SEPARATOR);
 
     public static Map<String, String> toMap(String propertyList) {
         Map<String, String> properties = new HashMap<String, String>();
         if (StringUtils.isNotEmpty(propertyList)) {
-            String[] items = propertyList.split(itemSeparatorQuoted);
+            String[] items = propertyList.split(ITEM_SEPARATOR_QUOTED);
             for (String item : items) {
                 if (StringUtils.isNotEmpty(item)) {
-                    String[] parts = item.split(valueSeparatorQuoted);
+                    String[] parts = item.split(VALUE_SEPARATOR_QUOTED);
                     if (parts.length == 2) {
                         properties.put(parts[0], parts[1]);
                     } else {
@@ -36,7 +36,7 @@ public class Utils {
     public static String toProperyList(Map<String, String> propertyMap) {
         final StringBuffer stringBuffer = new StringBuffer("");
         for (Map.Entry entry : propertyMap.entrySet()) {
-            stringBuffer.append(entry.getKey()).append(valueSeparator).append(entry.getValue()).append(itemSeparator);
+            stringBuffer.append(entry.getKey()).append(VALUE_SEPARATOR).append(entry.getValue()).append(ITEM_SEPARATOR);
         }
         return stringBuffer.toString();
     }
