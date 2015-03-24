3ec932c8ee08fc36774dd557e55af886f13ab3df
Oana.Mihai <oana.mihai@teamnet.ro>
2015-03-24 19:37:45 +0200

    replace println with logging; scan method parameters using java 7

--- wfmc-audit/src/main/java/ro/teamnet/wfmc/audit/aop/WfmcAuditAspect.java
+++ wfmc-audit/src/main/java/ro/teamnet/wfmc/audit/aop/WfmcAuditAspect.java
@@ -6,43 +6,45 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.reflect.MethodSignature;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ro.teamnet.wfmc.audit.annotation.Auditable;
 
 import java.lang.reflect.Method;
-//import java.lang.reflect.Parameter;
 
 /**
  * Audit aspect.
  */
-
 @Aspect
 public class WfmcAuditAspect {
 
+    Logger log = LoggerFactory.getLogger(WfmcAuditAspect.class);
+
     @Before("execution(* *(..)) && @annotation(auditable))")
     public void beforeAuditable(Auditable auditable) {
-        System.out.println("++++++++++++++++++++++++++++ Before audit : " + auditable.value());
+        log.info("Before audit : " + auditable.value());
     }
 
     @After("execution(* *(..)) && @annotation(auditable))")
     public void afterAuditable(Auditable auditable) {
-        System.out.println("++++++++++++++++++++++++++++ After audit : " + auditable.value());
+        log.info("After audit : " + auditable.value());
     }
 
     @Around("execution(* *(..)) && @annotation(auditable))")
     public Object wrapAroundAuditable(ProceedingJoinPoint pjp, Auditable auditable) throws Throwable {
-        System.out.println("############################################\nStarted auditing : " + auditable.value());
+        log.info("Started auditing : " + auditable.value());
         MethodSignature methodSignature = (MethodSignature) pjp.getSignature();
         Method auditedMethod = methodSignature.getMethod();
-        System.out.printf("Class: %s\nMethod name: %s\nReturn type: %s\nArguments:\n",
-                pjp.getSourceLocation().getWithinType().getName(), auditedMethod.getName(), methodSignature.getReturnType().getName());
-        //Parameter[] parameters = auditedMethod.getParameters();
-//        for (int i = 0; i < parameters.length; i++) {
-//            Parameter parameter = parameters[i];
-//            Object argumentValue = Class.forName(parameter.getType().getName()).cast(pjp.getArgs()[i]);
-//            System.out.println("\t" + parameter.getName() + " = " + argumentValue);
-//        }
-        Object returnValue = pjp.proceed(pjp.getArgs());
-        System.out.println("############################################\nFinished auditing : " + auditable.value());
+        log.info("Class: {}\nMethod name: {}\nReturn type: {}", pjp.getSourceLocation().getWithinType().getName(),
+                auditedMethod.getName(), methodSignature.getReturnType().getName());
+        Class<?>[] parameterTypes = auditedMethod.getParameterTypes();
+        Object[] args = pjp.getArgs();
+        for (int i = 0; i < parameterTypes.length; i++) {
+            Object argumentValue = Class.forName(parameterTypes[i].getName()).cast(args[i]);
+            log.info("arg" + i + " = " + argumentValue);
+        }
+        Object returnValue = pjp.proceed(args);
+        log.info("Finished auditing : " + auditable.value());
         return returnValue;
     }
 }
