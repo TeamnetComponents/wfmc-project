b7fce589aac3bfbefb132a25267414ffa36723c5
Oana.Mihai <oana.mihai@teamnet.ro>
2015-04-03 17:17:14 +0300

    removed wfmc-spring-services-elo module

--- wfmc-spring-services-elo/src/main/java/ro/teamnet/wfmc/elo/service/WfmcAuditedServiceEloImpl.java
+++
@@ -1,9 +0,0 @@
-package ro.teamnet.wfmc.elo.service;
-
-import ro.teamnet.wfmc.service.WfmcAuditedService;
-
-/**
- * Created by Oana.Mihai on 4/3/2015.
- */
-public class WfmcAuditedServiceEloImpl {//implements WfmcAuditedService {
-}
