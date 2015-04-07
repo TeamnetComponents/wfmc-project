148018be4be239e0f88d1bb894c1c1ba14cb1f96
Cristian D <criss.dumitru.87@gmail.com>
2015-04-07 17:47:08 +0300

    Audit for createProcessInstance without currentDate Sample

--- wfmc-spring-services/src/test/java/ro/teamnet/wfmc/service/WfmcAuditedServiceTest.java
+++ wfmc-spring-services/src/test/java/ro/teamnet/wfmc/service/WfmcAuditedServiceTest.java
@@ -3,28 +3,43 @@ package ro.teamnet.wfmc.service;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 import org.wfmc.wapi.WMConnectInfo;
 import org.wfmc.wapi.WMWorkflowException;
+import ro.teamnet.wfmc.audit.domain.WMEventAudit;
+import ro.teamnet.wfmc.audit.domain.WMEventAuditProcessInstance;
 import ro.teamnet.wfmc.service.mock.WfmcServiceMockImpl;
+import ro.teamnet.wfmc.audit.domain.WMProcessInstanceAudit;
+import ro.teamnet.wfmc.audit.service.WfmcAuditQueryService;
 
 import javax.inject.Inject;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = {WfmcAuditTestApplication.class})
 @IntegrationTest
-@Transactional("wfmcAuditTransactionManager")
 public class WfmcAuditedServiceTest {
+
+    @Inject
+    private WfmcAuditQueryService wfmcAuditQueryService;
     @Inject
     private WfmcAuditedService wfmcService;
+
+
+    private Logger log = LoggerFactory.getLogger(WfmcAuditedServiceTest.class);
 
     @Test
     public void testCreateProcessInstance() throws WMWorkflowException {
         wfmcService.connect(new WMConnectInfo("Andra@Administrator", "elo@RENNS2015", "Wfmc Test", "http://10.6.38.90:8080/ix-elo/ix"));
         wfmcService.createProcessInstance("1", "my procInstName");
+        WMProcessInstanceAudit wmProcessInstanceAudit = wfmcAuditQueryService.findWMProcessInstanceAuditByProcessDefinitionBusinessName("my procInstName");
+        WMEventAudit wmEventAudit = wfmcAuditQueryService.findWMEventAuditByUsername("Gogu");
+        log.info("Process definition id: {}", wmProcessInstanceAudit.getProcessDefinitionId());
+        log.info("Process instance id: {}", wmProcessInstanceAudit.getProcessInstanceId());
         wfmcService.disconnect();
     }
 
