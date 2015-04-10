3c4d51ca45fde8feeb4fb5f01e726c13c7569438
Oana.Mihai <oana.mihai@teamnet.ro>
2015-04-10 14:33:17 +0300

    moved the core audit features to separate jar

--- wfmc-audit/src/main/java/ro/teamnet/audit/strategy/MethodAuditingStrategyFactory.java
+++
@@ -1,8 +0,0 @@
-package ro.teamnet.audit.strategy;
-
-/**
- * A factory for creating MethodAuditingStrategy instances.
- */
-public interface MethodAuditingStrategyFactory {
-    MethodAuditingStrategy getStrategy(String methodName);
-}
